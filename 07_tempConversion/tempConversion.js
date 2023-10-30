bf04e4f4c0b55c1a95d21132582f1dfde21ff4da
LunevaDariaa <daria.safri@gmail.com>
2023-10-30 17:47:29 -0400

    Completed all 7 exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const tempConverted = (Number(temp) - 32) * 0.555555555;
+  const tempRounded = Number(tempConverted.toFixed(1));
+  return tempRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const tempConverted1 = Number(temp) * 1.8 + 32;
+  const tempRounded = Number(tempConverted1.toFixed(1));
+  return tempRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
